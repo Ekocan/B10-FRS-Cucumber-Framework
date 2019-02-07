3a22a82195647417a0a981a836c13e1297fe24e6
Ekrem <metrozi2018@Gmail.com>
2019-02-07 12:09:13 -0500

    implemented login feature, created page objects and implemented step definitions

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BrowerUtils {
+public class BrowserUtils {
 
     public static void wait(int seconds){
         try{
